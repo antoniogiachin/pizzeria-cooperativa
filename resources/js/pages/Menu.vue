8f1a05da8d87457e20d21811ab41045ec0c707cd
antoniogiachin <giachinantonio@outlook.com>
2022-04-20 23:17:59 +0200

    aggiunta rotta per singola pizza, recpero con axios menu e stmapo in page menu

--- resources/js/pages/Menu.vue
+++ resources/js/pages/Menu.vue
@@ -1,10 +1,40 @@
 <template>
-  <h1>Menu: le nostre specialità!</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-3 m-2" v-for="(pizza,index) in menu" :key="index">
+                <div class="card">
+                    <img src="https://picsum.photos/200/300" class="card-img-top" :alt="pizza.name">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ pizza.name }}</h5>
+                        <p class="card-text">Prezzo: {{pizza.price}}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Menu'
+    name: 'Menu',
+    data() {
+        return {
+            menu: [],
+        }
+    },
+    methods: {
+        getMenu() {
+            axios.get('/api/menu')
+            .then(response =>{
+                this.menu = response.data.results;
+                console.log(this.menu);
+            })
+        }
+    },
+    mounted () {
+        this.getMenu();
+    },
 }
 </script>
 
