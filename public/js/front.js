78a83ff37a99d2fd16d608e2ff4c4087a22b4d47
antoniogiachin <giachinantonio@outlook.com>
2022-04-21 17:07:28 +0200

    prova

--- public/js/front.js
+++ public/js/front.js
@@ -20644,7 +20644,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/alicecolombari/Desktop/GIT-PIZZA/pizzeria-cooperativa/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/antoniogiachin/Downloads/pizzeria-cooperativa/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
