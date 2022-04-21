8520d511b31967b3ed9f4e8fea9543485739d21d
Marco Sandri <sandrimarco98@gmail.com>
2022-04-21 10:24:29 +0200

    Admin blade show

--- resources/views/admin/pizzas/show.blade.php
+++ resources/views/admin/pizzas/show.blade.php
@@ -1,5 +1,18 @@
 @extends('admin.layouts.base')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+
+                <h1>{{$pizza->name}}</h1>
+
+                <div><strong>Prezzo: </strong> {{$pizza->price}} </div>
+                <div><strong>Slug: </strong> {{$pizza->slug}} </div>
+
+                <a href="{{url()->previous()}}" class="btn btn-primary">Torna indietro</a>
+
+            </div>
+        </div>
+    </div>
 @endsection
