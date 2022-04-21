601f9a45fcb72ce6770a339fb6dd2f79dae4e60e
AliceColombari <colombari.23@gmail.com>
2022-04-21 10:02:26 +0200

    creato nuovo oggetto per invio email + vista email ricevuto mailtrap bozza

---
+++ resources/views/emails/new-contact.blade.php
@@ -0,0 +1,10 @@
+<h1>Ciao Admin!</h1>
+<p>
+    Hai ricevuto un nuovo contatto dal sito <br>
+    Ecco i dettagli: <br>
+    Nome del contatto: {{$lead->name}} <br>
+    Email del contatto: {{$lead->email}} <br>
+    Messaggio: {{$lead->message}} <br>
+</p>
+
+{{-- inserisci grafica carina per ricezione email --}}
