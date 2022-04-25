8a2b6c268db69f8b11035a2a02b1b0ab79b729f9
pietroline <pietrobarberi@hotmail.it>
2022-04-25 22:02:09 +0200

    fix no records back end

--- resources/views/admin/pizzas/index.blade.php
+++ resources/views/admin/pizzas/index.blade.php
@@ -29,39 +29,44 @@
             </form>
        </div>
        
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Pizza</th>
-            <th scope="col">Price</th>
-            <th scope="col">Slug</th>
-            <th scope="col">Category</th>
-        </tr>
-        </thead>
-        <tbody>
-            @foreach ($pizzas as $pizza)
+       @if(count($pizzas))
+            <thead>
                 <tr>
-                    <td>{{$pizza->id}}</td>
-                    <td><a href="{{route('admin.pizzas.show', $pizza->id)}}">{{$pizza->name}}</a></td>
-                    <td>{{$pizza->price}}</td>
-                    <td>{{$pizza->slug}}</td>
-                    <td>{{isset($pizza->category) ? $pizza->category->name : "NULL"}}</td>
-                    <td class="d-flex">    
-                        <a href="{{route('admin.pizzas.show', $pizza->id)}}" class="btn btn-primary m-2">View</a>
-                        <a href="{{route('admin.pizzas.edit', $pizza->id)}}" class="btn btn-secondary m-2">Edit</a>
-
-                        <form method="POST" action="{{route('admin.pizzas.destroy', $pizza->id)}}">
-                        
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit" class="btn btn-danger m-2">DELETE</button>
-                        
-                        </form>
-                    </td>
+                    <th scope="col">#</th>
+                    <th scope="col">Pizza</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Category</th>
                 </tr>
-            @endforeach
-        </tbody>
+            </thead>
+            <tbody>
+                @foreach ($pizzas as $pizza)
+                    <tr>
+                        <td>{{$pizza->id}}</td>
+                        <td><a href="{{route('admin.pizzas.show', $pizza->id)}}">{{$pizza->name}}</a></td>
+                        <td>{{$pizza->price}}</td>
+                        <td>{{$pizza->slug}}</td>
+                        <td>{{isset($pizza->category) ? $pizza->category->name : "NULL"}}</td>
+                        <td class="d-flex">    
+                            <a href="{{route('admin.pizzas.show', $pizza->id)}}" class="btn btn-primary m-2">View</a>
+                            <a href="{{route('admin.pizzas.edit', $pizza->id)}}" class="btn btn-secondary m-2">Edit</a>
+
+                            <form method="POST" action="{{route('admin.pizzas.destroy', $pizza->id)}}">
+                            
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button type="submit" class="btn btn-danger m-2">DELETE</button>
+                            
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+
+        @else
+            <div> Non ci sono pizze appartenenti alla categoria selezionata</div>
+        @endif
     </table>
 </div>
     
